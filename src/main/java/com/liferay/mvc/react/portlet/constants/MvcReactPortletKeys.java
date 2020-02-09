26582760760cdc129be6535c95bdc47ba8696212
Evan Thibodeau <evan.thibodeau@liferay.com>
2020-02-09 14:43:07 +0400

    blade init mvc-portlet

---
+++ src/main/java/com/liferay/mvc/react/portlet/constants/MvcReactPortletKeys.java
@@ -0,0 +1,11 @@
+package com.liferay.mvc.react.portlet.constants;
+
+/**
+ * @author evanthibodeau
+ */
+public class MvcReactPortletKeys {
+
+	public static final String MVCREACT =
+		"com_liferay_mvc_react_portlet_MvcReactPortlet";
+
+}
---
+++ src/main/java/com/liferay/mvc/react/portlet/portlet/MvcReactPortlet.java
@@ -0,0 +1,30 @@
+package com.liferay.mvc.react.portlet.portlet;
+
+import com.liferay.mvc.react.portlet.constants.MvcReactPortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author evanthibodeau
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=MvcReact",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + MvcReactPortletKeys.MVCREACT,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class MvcReactPortlet extends MVCPortlet {
+}
